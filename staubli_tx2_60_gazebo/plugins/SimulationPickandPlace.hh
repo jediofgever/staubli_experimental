9d1823e91154b7e29be4b298af88d6a3fe6da64a
jediofgever <fetulahatas1@gmail.com>
2020-07-20 13:15:31 +0800

    remove unused dep

--- staubli_tx2_60_gazebo/plugins/SimulationPickandPlace.hh
+++ staubli_tx2_60_gazebo/plugins/SimulationPickandPlace.hh
@@ -12,13 +12,11 @@
 #include <gazebo/gazebo.hh>
 #include <gazebo/physics/physics.hh>
 #include <ignition/math4/ignition/math/Vector3.hh>
-
-#include <staubli_arm_perception_utilities/pickplace/PickandPlacer.h>
 #include <geometry_msgs/Pose.h>
 #include <geometry_msgs/PoseArray.h>
 #include <moveit/move_group_interface/move_group_interface.h>
+#include <tf/transform_listener.h>
 
-#include <staubli_arm_perception_utilities/utils.h>
 #include <jsk_recognition_msgs/BoundingBox.h>
 #include <jsk_recognition_msgs/BoundingBoxArray.h>
 
